728c38773f85827494a792b08f150fddfb98fbbd
amousaid <amousaid@c4r9p8.1337.ma>
2023-12-16 06:58:03 +0100

    js

--- main.js
+++ main.js
@@ -2,9 +2,32 @@ function getnum()
 {
     var num1 = document.getElementById("Number1").value;
     var num2 = document.getElementById("Number2").value;
+    var forms = document.getElementById("forms").value;
 
     var result;
-    result = Number(num1) + Number(num2);
+    
 
-    window.alert("Result: " + result);
+    switch (forms)
+    {
+        case "+":
+            result = Number(num1) + Number(num2);
+            break;
+        case "-":
+            result = Number(num1) - Number(num2);
+            break;
+        case "*":
+            result = Number(num1) * Number(num2);
+            break;
+        case "/":
+            result = Number(num1) / Number(num2);
+            break;
+        case "^":
+            result = Number(num1) ** Number(num2);
+            break;
+    }
+    var resultContainer = document.getElementById('resultContainer');
+    var resultElement = document.createElement('p');
+    resultElement.textContent = 'Result: ' + result;
+    resultContainer.innerHTML = '';
+    resultContainer.appendChild(resultElement);
 }
