1bb6ca6daa1f796265b80301a83ddec72be4039d
Abdilah Mousaid <51842546+abdilahmusid@users.noreply.github.com>
2023-12-23 14:37:30 +0100

    finish

--- main.js
+++ main.js
@@ -1,91 +1,21 @@
-let result = 0;
-let temp = 0;
-let num = 0;
-let chyata = 0;
-function appendValue(number) 
-{
-    document.getElementById('result').value += number;
-    num += number;
+let result = document.getElementById('result');
+
+function appendValue(value) {
+    result.value += value;
 }
-var sign; 
-function calculator(forms)
-{
-    if (temp != 0)
-    {
-        getResult2();
-        num = result;
-    }
-    if(chyata != 0)
-    {
-        temp = chyata;
-    }
-    else
-    {
-        temp = num;
-    }
-    sign = forms;
-    num = '';
-    document.getElementById('result').value = '';
+
+function calculator(operator) {
+    result.value += operator;
 }
+
 function clearResult() {
-    num = 0;
-    temp = 0;
-    result = 0;
-    chyata = 0;
-    document.getElementById('result').value = '';
+    result.value = '';
 }
-function getResult()
-{
-    document.getElementById('result').value = '';
-    switch (sign)
-    {
-        case "+":
-            result = Number(temp) + Number(num);
-            break;
-        case "-":
-            result = Number(temp) - Number(num);
-            break;
-        case "*":
-            result = Number(temp) * Number(num);
-            break;
-        case "/":
-            if(Number(num) == 0)
-            {
-                result = NaN;
-                break;
-            }
-            result = Number(temp) / Number(num);
-            break;
-        case "^":
-            result = Number(temp) ** Number(num);
-            break;
+
+function getResult() {
+    try {
+        result.value = eval(result.value);
+    } catch (error) {
+        result.value = 'Error';
     }
-    document.getElementById('result').value =   Number(result);
-    chyata = result;
-    num = 0;
-    temp = 0;
-    result = 0;
-}
-function getResult2()
-{
-    document.getElementById('result').value = '';
-    switch (sign)
-    {
-        case "+":
-            result = Number(temp) + Number(num);
-            break;
-        case "-":
-            result = Number(temp) - Number(num);
-            break;
-        case "*":
-            result = Number(temp) * Number(num);
-            break;
-        case "/":
-            result = Number(temp) / Number(num);
-            break;
-        case "^":
-            result = Number(temp) ** Number(num);
-            break;
-    }
-    temp = 0;
 }
